c23942ffcf6ea2d437ecea484c200410b5b20525
Ranjeet <ranjeet@drupaltechie.com>
2024-10-13 23:25:42 +0530

    code update

---
+++ src/app/dashboard/parent/page.tsx
@@ -0,0 +1,9 @@
+const Parant = () => {
+  return (
+    <div>
+      <h1>Parent Component</h1>
+      {/* <Child /> */}
+    </div>
+  );
+}
+export default Parant; 
---
+++ src/app/dashboard/stduent/page.tsx
@@ -0,0 +1,9 @@
+const Student = () => {
+  return (
+    <div>
+      <h1>Student</h1>
+      <p>Student Page</p>
+    </div>
+  );
+}
+export default Student;
